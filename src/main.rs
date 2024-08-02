bdd113026f3165d742c3c13b1112225bc2c3a8b7
bwintermann <bjarne.wintermann@me.com>
2024-08-02 16:37:23 +0200

    Proc macro can error if used wrongly

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ extern crate xrt_proc_macro;
 use xrt_proc_macro::*;
 
 #[kernel(add_kernel, abc)]
-struct MyStruct;
+pub struct MyStruct {};
 
 fn main() {
     let values = get_arguments("hls/vscale_f32_sw_emu.xclbin", "vscale_f32").unwrap();
     for arg in values {
         println!("{}  |  {}  |  {}", arg["name"], arg["type"], arg["size"]);
     }
+
+    let ms = MyStruct;
+    println!("{}", ms.ans());
 }
--- xrt_proc_macro/src/lib.rs
+++ xrt_proc_macro/src/lib.rs
@@ -1,12 +1,27 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
-use syn::{self, parse_macro_input, AttributeArgs};
+use quote::quote;
+use syn::{self, parse_macro_input, spanned::Spanned, token::Token, AttributeArgs, DataStruct, ItemStruct};
 
 #[proc_macro_attribute]
 pub fn kernel(attrs: TokenStream, items: TokenStream) -> TokenStream {
     let attributes = parse_macro_input!(attrs as AttributeArgs);    
     if attributes.len() != 1 {
-        return syn::Error::new(attrs, "To create a kernel argument handler function provide exactly one argument: The name of the kernel: #[kernel(my_add_kernel)]").to_compile_error().into();
+        return syn::Error::new(attributes[0].span(), "To create a kernel argument handler function provide exactly one argument: The name of the kernel: #[kernel(my_add_kernel)]").to_compile_error().into();
     }
-    items
+
+    let parsed_struct = parse_macro_input!(items as ItemStruct);
+    let struct_name = parsed_struct.ident;
+
+    let result = quote! {
+        impl #struct_name {
+            fn ans() -> i32 {
+                return 42;
+            }
+        }
+    };
+    let mut final_stream = TokenStream::new();
+    final_stream.extend(&items.clone().into_iter());
+    final_stream.extend::<proc_macro::TokenStream>(result.into());
+    final_stream
 }
