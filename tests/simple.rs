73b95ab0646593b94c35da3de8f0e8345bcf55df
Gerrit Pape <papeg@mail.upb.de>
2024-08-03 15:34:18 +0200

    create test for simple api

--- tests/simple.rs
+++ tests/simple.rs
@@ -2,9 +2,10 @@ extern crate xrt_proc_macro;
 use xrt_proc_macro::*;
 
 #[kernel("hls/vscale_u32_sw_emu.xclbin", "vscale_u32")]
-pub struct MyStruct;
+pub struct VScaleU32;
 
-fn main() {
-    let ms = MyStruct;
+#[test]
+fn run_vscale_simple_u32() {
+    let ms = VScaleU32;
     println!("{}", ms.ans());
 }
